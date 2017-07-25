9a5ec5de7c0f76dbbec932037ebb6ba7d49df3cb
Louis Mau <lmau404@mtroyal.ca>
2017-07-25 12:01:04 -0700

    Added the file reading

--- Src/src/Main.java
+++ Src/src/Main.java
@@ -2,24 +2,40 @@ package src;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
 
     public static void main(String[] args){
 
-        BufferedWriter bufferedWriter;
-        BufferedReader bufferedReader;
+        BufferedWriter upload;
+        BufferedWriter merge;
+        BufferedReader input;
 
         try
         {
-            bufferedReader = new BufferedReader();
-            bufferedWriter = new BufferedWriter();
+            input = Files.newBufferedReader(new File("C:\\Users\\Louis\\Desktop\\Customer Cleanup.csv").toPath());
+            upload = Files.newBufferedWriter(new File("C:\\Users\\Louis\\Desktop\\Upload.csv").toPath());
+            merge = Files.newBufferedWriter(new File("C:\\Users\\Louis\\Desktop\\Merge.csv").toPath());
+
+            String inputLine = input.readLine();
+            upload.write(inputLine+"\n");       //Write header for the file
+            merge.write(inputLine+"\n");       //Write header for the file
+
+            while(inputLine != null) {
+
+
+
+                inputLine = input.readLine();
+            }
+
 
         }
         catch (IOException io)
         {
-            System.out.print("File not found");
+            System.out.print("File not found " + io.getMessage());
         }
 
 
