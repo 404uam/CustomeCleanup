efd68300514eca0225ad423a37c3846662396098
Louis Mau <lmau404@mtroyal.ca>
2017-07-25 12:53:17 -0700

    Created switch case for different types of infomration coming in

--- Src/src/Person.java
+++ Src/src/Person.java
@@ -20,7 +20,20 @@ public class Person {
     type = "";
     public Person(String[] person) {
 
-
+        switch (person.length)
+        {
+            case 16: //No Email
+                break;
+            case 21: //Has Email
+                break;
+            case 25: //Has a note
+                break;
+            case 26: //has a customer type
+                break;
+            default:
+                System.out.println("Unexpected person length found in file");
+                break;
+        }
 
     }
 }
